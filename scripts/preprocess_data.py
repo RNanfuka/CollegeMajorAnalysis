cfdfb165e123d5d8c330c26e472453ad34437911
Alan Liu <alnliu2000@gmail.com>
2025-12-12 00:13:18 -0800

    Abtract preprocess_data.py

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,36 +1,10 @@
+import os
 import pandas as pd
 import click
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-# Preprocessing
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-
-def get_feature_names(preprocessor):
-    feature_names = []
-
-    # Ordinal features
-    ordinal_features = preprocessor.transformers_[0][2]
-    feature_names.extend(ordinal_features)
-
-    # Binary features
-    binary_features = preprocessor.transformers_[1][2]
-    binary_encoder = preprocessor.transformers_[1][1].named_steps['encoder']
-    binary_feature_names = binary_encoder.get_feature_names_out(binary_features)
-    feature_names.extend(binary_feature_names)
-
-    # Categorical features
-    categorical_features = preprocessor.transformers_[2][2]
-    categorical_encoder = preprocessor.transformers_[2][1].named_steps['encoder']
-    categorical_feature_names = categorical_encoder.get_feature_names_out(categorical_features)
-    feature_names.extend(categorical_feature_names)
-
-    # Numeric features
-    numeric_features = preprocessor.transformers_[3][2]
-    feature_names.extend(numeric_features)
-
-    return feature_names
+from src.fit_transform import fit_transform
 
 # Main function
 @click.command()
@@ -48,8 +22,13 @@ def main(input_dir, out_dir):
     drop_features = ['fnlwgt', 'education-num', 'race']
     target = "income"
 
-    X_train = adult_df.drop(columns = drop_features + [target])
-    y_train = adult_df[target]
+    transformed_features = {
+        'numeric': numeric_features,
+        'categorical': categorical_features,
+        'ordinal': ordinal_features,
+        'binary': binary_features,
+        'drop': drop_features
+    }
 
     # Ordered levels for the education column (lowest to highest)
     education_order = [
@@ -61,56 +40,18 @@ def main(input_dir, out_dir):
         'Doctorate',
     ]
 
-    # Column Preprocessing
-    # Pipelines for different data types
-    categorical_pipeline = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OneHotEncoder(handle_unknown='ignore', sparse_output=False))
-    ])
-
-    ordinal_pipeline = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OrdinalEncoder(categories=[education_order], handle_unknown='use_encoded_value', unknown_value=-1))
-    ])
-
-    binary_pipeline = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('encoder', OneHotEncoder(drop='if_binary', handle_unknown='ignore', sparse_output=False))
-    ])
-
-    numeric_pipeline = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='median')),
-        ('scaler', StandardScaler())
-    ])
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('ordinal', ordinal_pipeline, ordinal_features),
-            ('binary', binary_pipeline, binary_features),
-            ('categorical', categorical_pipeline, categorical_features),
-            ('numeric', numeric_pipeline, numeric_features),
-        ]
-    )
-
-    # generate preprocessed training data just to check how it looks
-    X_train_processed = preprocessor.fit_transform(X_train)
-
-    # get the feature names after preprocessing
-    feature_names = get_feature_names(preprocessor)
-
     # convert processed data back to DataFrame for better readability
-    X_train_processed_df = pd.DataFrame(X_train_processed, columns=feature_names)
-    X_train_processed_df[target] = y_train.reset_index(drop=True)
+    processed_df = fit_transform(adult_df, transformed_features, education_order, target)
 
     #Output
     if "train" in input_dir:
-        X_train_processed_df.to_csv(out_dir + "preprocessed_train.csv", index=False)
+        processed_df.to_csv(out_dir + "preprocessed_train.csv", index=False)
         click.echo(f"Saved preprocessed_train.csv to file directory {out_dir}")
     elif "test" in  input_dir:
-        X_train_processed_df.to_csv(out_dir + "preprocessed_test.csv", index=False)
+        processed_df.to_csv(out_dir + "preprocessed_test.csv", index=False)
         click.echo(f"Saved preprocessed_test.csv to file directory {out_dir}")
     else:
-        X_train_processed_df.to_csv(out_dir + "preprocessed_data.csv", index=False)
+        processed_df.to_csv(out_dir + "preprocessed_data.csv", index=False)
         click.echo(f"Saved preprocessed_data.csv to file directory {out_dir}")
 
 if __name__ == '__main__':
